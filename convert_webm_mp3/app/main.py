90f94d8439ed442630be024c354048f6ae056b93
alexanghh <alexanghh@gmail.com>
2022-12-05 03:52:59 +0800

    Added code to convert webm to mp3 and upload to s3

--- convert_webm_mp3/app/main.py
+++ convert_webm_mp3/app/main.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+import ffmpeg
 import io
 import os
 from pynumaflow.function import Messages, Message, Datum, UserDefinedFunctionServicer
@@ -9,39 +10,44 @@ def map_handler(key: str, datum: Datum) -> Messages:
     val = datum.value
     _ = datum.event_time
     _ = datum.watermark
-    
+
     data = json.loads(val.decode("utf-8"))
     webm_s3_bucket = data['webm_s3_bucket']
     webm_s3_key = data['webm_s3_key']
-    
+
     print('downloading: ' + json.dumps(data))
     messages = Messages()
     try:
-      # setup s3
-      s3 = boto3.resource('s3',
-        endpoint_url = os.environ['MINIO_URL'],
-        aws_access_key_id = os.environ['MINIO_USER'],
-        aws_secret_access_key = os.environ['MINIO_SECRET']
-      )
-      
-      # TODO: download from s3
-      boto_webm_bucket = s3.Bucket(webm_s3_bucket)
-      
-      # TODO: convert webm to mp3 using ffmpeg
-      
+        # setup s3
+        s3_client = boto3.client('s3',
+                                 endpoint_url=os.environ['MINIO_URL'],
+                                 aws_access_key_id=os.environ['MINIO_USER'],
+                                 aws_secret_access_key=os.environ['MINIO_SECRET']
+                                 )
 
-      # TODO: upload to mp3
-      mp3_s3_bucket = 'mp3'
-      mp3_s3_key = webm_s3_key[:-4] + '.mp3'
-      # Get bucket object
-      boto_bucket = s3.Bucket(mp3_s3_bucket)
-      boto_bucket.upload_fileobj(buf, mp3_s3_key) # TODO: set mp3 file
-      print('uploaded to {}/{}'.format(s3_bucket, s3_key))
-      messages.append(Message.to_vtx('success', str.encode(json.dumps(data))))
+        webm_buf = io.BytesIO()
+        s3_client.download_fileobj(webm_s3_bucket, webm_s3_key, webm_buf)
+
+        out, _ = (
+            ffmpeg
+            .input('pipe:', vn=None)
+            .output('pipe:', format='mp3', metadata='title=' + webm_s3_key.split('/')[-1][:-4])
+            .overwrite_output()
+            .run(capture_stdout=True, input=webm_buf.getbuffer())
+        )
+        mp3_buf = io.BytesIO(out)
+
+        mp3_s3_bucket = 'mp3'
+        mp3_s3_key = webm_s3_key[:-4] + 'mp3'
+        s3_client.upload_fileobj(mp3_buf, mp3_s3_bucket, mp3_s3_key)
+        print('uploaded to {}/{}'.format(mp3_s3_bucket, mp3_s3_key))
+        data['mp3_s3_bucket'] = mp3_s3_bucket
+        data['mp3_s3_key'] = mp3_s3_key
+        messages.append(Message.to_vtx('success', str.encode(json.dumps(data))))
     except Exception as e:
-      print(e)
-      data['exception'] = str(e)
-      messages.append(Message.to_vtx('error', str.encode(json.dumps(data))))
+        print(e)
+        data['exception'] = str(e)
+        messages.append(Message.to_vtx('error', str.encode(json.dumps(data))))
     return messages
 
 

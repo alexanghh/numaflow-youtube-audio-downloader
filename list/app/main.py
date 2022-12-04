babcc9ed1e7a39611cec9b169ff5de25cad467cb
alexanghh <alexanghh@gmail.com>
2022-12-05 03:50:58 +0800

    Reformat code.

--- list/app/main.py
+++ list/app/main.py
@@ -7,22 +7,22 @@ def map_handler(key: str, datum: Datum) -> Messages:
     val = datum.value
     _ = datum.event_time
     _ = datum.watermark
-    
+
     data = json.loads(val.decode("utf-8"))
     playlist_url = data['playlist_url']
     print('listing: ' + json.dumps(data))
     messages = Messages()
     try:
-      p = Playlist(playlist_url)
-      for url in p.video_urls:
-        msg = {}
-        msg['playlist'] = p.title
-        msg['video_url'] = url
-        messages.append(Message.to_vtx('success', str.encode(json.dumps(msg))))
+        p = Playlist(playlist_url)
+        for url in p.video_urls:
+            msg = {}
+            msg['playlist'] = p.title
+            msg['video_url'] = url
+            messages.append(Message.to_vtx('success', str.encode(json.dumps(msg))))
     except Exception as e:
-      print(e)
-      data['exception'] = str(e)
-      messages.append(Message.to_vtx('error', str.encode(json.dumps(data))))
+        print(e)
+        data['exception'] = str(e)
+        messages.append(Message.to_vtx('error', str.encode(json.dumps(data))))
     return messages
 
 

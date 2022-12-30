1b970573132d659c393e83ba12cffb0b84faa808
alexanghh <alexanghh@gmail.com>
2022-12-30 18:08:21 +0800

    Added extra commands

--- help.sh
+++ help.sh
@@ -4,10 +4,18 @@ docker build -t test-python-udf:v5 . && kind load docker-image docker.io/library
 kubectl apply -f pipeline-numaflow.yaml
 # port forward http input source
 kubectl port-forward web-crawler-input-0-mlxmq 8444:8443
+curl -kq -X POST -d '{"playlist_url":"https://www.youtube.com/playlist?list=PLxRjZqOuEn4c85_km96U9Z0rwJI10txUU"}' https://localhost:8444/vertices/input
 # send data to http input source endpoint
 curl -kq -X POST -d "https://www.facebook.com/" https://localhost:8444/vertices/input
+
+kubectl logs youtube-playlist-dowload-download-youtube-save-0-szj16 numa
+
+# delete flow
+kubectl delete -f pipeline-numaflow.yaml
 
 # port forward numaflow ui
 kubectl -n numaflow-system port-forward deployment/numaflow-server 8443:8443
 
-
+# setup numaflow-system
+kubectl create namespace numaflow-system
+kubectl apply -n numaflow-system -f https://raw.githubusercontent.com/numaproj/numaflow/v0.7.0-rc1/config/install.yaml
